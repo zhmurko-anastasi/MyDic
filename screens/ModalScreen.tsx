68305e4995501d00dd23122bc7d9bb6ab7399f5c
Anastasiia Zhmurko <zhana141281@gmail.com>
2021-10-04 10:09:38 +0300

    initial commit

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -2,8 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from '../src/components/EditScreenInfo';
+import { Text, View } from '../src/components/Themed';
 
 export default function ModalScreen() {
   return (
